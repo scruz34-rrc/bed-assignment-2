72ddcb6edd7e37a3e8a9f1483444f7414590116a
Silverio Cruz <scruz34-rrc>
2026-02-05 02:32:10 -0600

    Added interface for the tickets.

---
+++ src/api/v1/services/ticketService.ts
@@ -0,0 +1,8 @@
+export interface Ticket {
+    id: number;
+    title: string;
+    description: string;
+    priority: "critical" | "high" | "medium" | "low";
+    status: "open" | "in-progress" | "resolved";
+    createdAt: string;
+}
